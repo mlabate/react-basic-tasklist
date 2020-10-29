8edb1025ea8e4a951a1fccfb6b6e9fcfcd275b67
Mario Labate <labate.mario@gmail.com>
2020-10-29 12:11:17 +0100

    backend

---
+++ backend/test_data.js
@@ -0,0 +1,29 @@
+const DAILY_TASKS = [
+    {
+      task: "Make Coffee",
+      completed: false,
+      id: 1
+    },
+    {
+      task: "Learn GraphQl",
+      completed: false,
+      id: 2
+    },
+    {
+      task: "Buy pizza",
+      completed: false,
+      id: 3
+    },
+    {
+      task: "Learn NodeJs",
+      completed: false,
+      id: 4
+    },
+    {
+      task: "Call parents",
+      completed: false,
+      id: 5
+    }
+  ];
+  
+  module.exports = DAILY_TASKS;
